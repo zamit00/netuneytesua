dddde59ae19f68c5c019de9022d15cb81d0e5516
zamit00 <zamit00@gmail.com>
2024-12-14 21:26:32 +0200

    Update netunimscript.js

--- netunimscript.js
+++ netunimscript.js
@@ -39,49 +39,97 @@ const dogma=`<Row>
 
 
 
-  var maslula= document.getElementById("maslul-type");
-  var moza=document.getElementById("product");
-  function changeka(){
-    for(let i=1;i<=3;i++){
-        document.getElementById(`thb${i}`).textContent='';
-        document.getElementById(`output${i}`).textContent='';
-   
-        document.getElementById(`thb${i+10}`).textContent='';
-        document.getElementById(`output${i+10}`).textContent='';
-         document.getElementById(`thb${i+20}`).textContent='';
-        document.getElementById(`output${i+20}`).textContent='';
-   
-    
-    }
-   }
-function specificadd(){
- 
- 
-  var maslul1= document.getElementById("maslul-type1");
-  var maslul2= document.getElementById("maslul-type2");
 
-const optionsa = maslul1.querySelectorAll('option');
-if(optionsa.length>1){
-for (let i = 1; i < optionsa.length; i++) {
-  maslul1.removeChild(optionsa[i]);
+const mozar=["קרנות השתלמות","תגמולים ואישית לפיצויים","קופת גמל להשקעה",
+    "קופת גמל להשקעה - חסכון לילד","מרכזית לפיצויים"];
+
+    const layeled=["חיסכון לילד -חוסכים המעדיפים סיכון מועט","חיסכון לילד -חוסכים המעדיפים סיכון בינוני",
+    "חיסכון לילד -חוסכים המעדיפים סיכון גבוה","הלכה יהודית"];
+    const hishtalmot=["אג\"ח ממשלות","אג\"ח סחיר","אשראי ואג\"ח","הלכה יהודית","כללי","כספי (שקלי)","מניות","מניות סחיר","משולב מניות","משולב סחיר","עוקב מדד s&p 500","עוקב מדדי אג\"ח","עוקב מדדי מניות","עוקב מדדים - גמיש"];
+    const gemel=["50-60","אג\"ח ממשלות","אג\"ח סחיר","אשראי ואג\"ח","הלכה יהודית","50-60","כספי (שקלי)","מניות ","מניות סחיר","60 ומעלה","משולב סחיר","עוקב מדד s&p 500","עוקב מדדי אג\"ח","עוקב מדדי מניות","עוקב מדדים - גמיש"];
+    const merkazitlepizuyim=["כללי"];
+
+  var selectElement;
+  var maslul= document.getElementById("maslul-type");
+  var moz=document.getElementById("product");
+  
+mozar.forEach(item => {
+  const option = document.createElement("option");
+  option.value = item;  // value יהיה שם הרכיב
+  option.textContent = item;  // textContent יהיה שם הרכיב
+  moz.appendChild(option);  // הוספת ה-option ל-select
+});
+
+hishtalmot.sort((a, b) => a.localeCompare(b));
+hishtalmot.forEach(item => {
+  const optiona = document.createElement("option");
+  optiona.value = item;  // value יהיה שם הרכיב
+  optiona.textContent = item;  // textContent יהיה שם הרכיב
+  maslul.appendChild(optiona);  // הוספת ה-option ל-select
+});
+function changek(){
+ for(let i=1;i<=3;i++){
+     document.getElementById(`thb${i}`).textContent='';
+     document.getElementById(`output${i}`).textContent='';
+
+     document.getElementById(`thb${i+10}`).textContent='';
+     document.getElementById(`output${i+10}`).textContent='';
+      document.getElementById(`thb${i+20}`).textContent='';
+     document.getElementById(`output${i+20}`).textContent='';
+
+ 
+ }
 }
+
+function rashiadd(){
+ changek();
+const options = maslul.querySelectorAll('option');
+for (let i = 1; i < options.length; i++) {
+  maslul.removeChild(options[i]);
 }
-  const optionsb = maslul2.querySelectorAll('option');
-if(optionsb.length>1){
-for (let i = 1; i < optionsb.length; i++) {
-  maslul2.removeChild(options[i]);
+var rashit;
+  
+    if(moz.value==="קרנות השתלמות"){rashit=hishtalmot;}
+    if(moz.value==="תגמולים ואישית לפיצויים"){rashit=gemel;}
+    if(moz.value==="קופת גמל להשקעה"){rashit=hishtalmot;}
+    if(moz.value==="קופת גמל להשקעה - חסכון לילד"){rashit=layeled;}
+    if(moz.value==="מרכזית לפיצויים"){rashit=merkazitlepizuyim;}
+    rashit.sort((a, b) => a.localeCompare(b));
+    rashit.forEach(item => {
+    const option = document.createElement("option");
+    option.value = item;  // value יהיה שם הרכיב
+    option.textContent = item;  // textContent יהיה שם הרכיב
+    maslul.appendChild(option);  // הוספת ה-option ל-select
+});
+  
 }
-} 
+
+
+
+function bring(){
+ for(let i=1;i<=3;i++){
+     document.getElementById(`thb${i}`).textContent='';
+     document.getElementById(`output${i}`).textContent='';
+     document.getElementById(`thb${i+10}`).textContent='';
+     document.getElementById(`output${i+10}`).textContent='';
+     document.getElementById(`thb${i+20}`).textContent='';
+     document.getElementById(`output${i+20}`).textContent='';
+
+ 
+ }
+ 
  fetch('kupotKlali.xml')
     .then(response => response.text()) // מקבל את תוכן הקובץ כטקסט
     .then(xmlString => {
+
         const parser = new DOMParser(); // יוצר parser לניתוח XML
         const xmlDoc = parser.parseFromString(xmlString, "application/xml"); // מנתח את הטקסט למבנה XML
+        
    
-     
+
       const rows = xmlDoc.getElementsByTagName("Row");
       let optionCollection = [];
-    for (let i = 0; i < rows.length; i++) {
+      for (let i = 0; i < rows.length; i++) {
         const row = rows[i];
         
         
@@ -89,60 +137,52 @@ for (let i = 1; i < optionsb.length; i++) {
         const yitratnehasim = row.getElementsByTagName("YITRAT_NCHASIM_LSOF_TKUFA")[0]?.textContent || '';
         const divuach=  row.getElementsByTagName("MATZAV_DIVUACH")[0]?.textContent ||  '';
         const ochlosiyayaad=row.getElementsByTagName("UCHLUSIYAT_YAAD")[0]?.textContent ||  '';
-        const mozara = row.getElementsByTagName("SUG_KUPA")[0]?.textContent ||  '';
+        const mozar = row.getElementsByTagName("SUG_KUPA")[0]?.textContent ||  '';
+
         const shemkupa = row.getElementsByTagName("SHM_KUPA")[0]?.textContent || '';
         const mhkupa = row.getElementsByTagName("ID")[0]?.textContent ||  '';
-        const masa = row.getElementsByTagName("HITMAHUT_MISHNIT")[0]?.textContent ||  '';
-        const tesuama = row.getElementsByTagName("TSUA_MITZTABERET_LETKUFA")[0]?.textContent ||  '';
+        const mas = row.getElementsByTagName("HITMAHUT_MISHNIT")[0]?.textContent ||  '';
+        const tesuam = row.getElementsByTagName("TSUA_MITZTABERET_LETKUFA")[0]?.textContent ||  '';
        const tesuam36 = row.getElementsByTagName("TSUA_MITZTABERET_36_HODASHIM")[0]?.textContent ||  '';
        const tesuam60 = row.getElementsByTagName("TSUA_MITZTABERET_60_HODASHIM")[0]?.textContent ||  '';
 
 
-    
+     
         if(Number(yitratnehasim)>0 &&  divuach ==="דווח" &&
         ochlosiyayaad!== "עובדי סקטור מסויים" && ochlosiyayaad!==`עובדי מפעל/גוף מסויים`
-         && moza.value && maslula.value!=='' && mozara!=='מטרה אחרת' && Number(tesuama)!==0
-        && tesuama){ 
+         && moz.value && maslul.value!=='' && mozar!=='מטרה אחרת' && Number(tesuam)!==0
+        && tesuam){
 
         
-        if(mozara===moza.value && masa===maslula.value ) {
-          const optiona = document.createElement("option");
-          optiona.value = mhkupa;  // value יהיה שם הרכיב
-          optiona.textContent = shemkupa;  // textContent יהיה שם הרכיב
-          maslul1.appendChild(optiona); 
-            
-          const optionb = document.createElement("option");
-          optionb.value = mhkupa;  // value יהיה שם הרכיב
-          optionb.textContent = shemkupa;  // textContent יהיה שם הרכיב
-          maslul2.appendChild(optionb); 
-              
-            }
-           
-            
-
-           
-            
-           
+        if(mozar===moz.value && mas===maslul.value ) {
+            optionCollection.push({mh:mhkupa, shemkupa: shemkupa,mozar:mozar, tesuam: Number(tesuam), 
+              ochlosiyayaad: ochlosiyayaad, divuach: divuach, tesuam36:Number(tesuam36),tesuam60:Number(tesuam60) });
           }
-        
+        }
       }
       
     
      
       if(optionCollection.length!==0){
-      for(let r=0;r<=optionCollection.length;r++){
-        const option = document.createElement("option");
-        option.value = optionCollection.mh;  // value יהיה שם הרכיב
-        option.textContent = optionCollection.shemkupa;  // textContent יהיה שם הרכיב
-         maslul1.appendChild(option);
-        
-        const option2 = document.createElement("option");
-        option2.value = optionCollection.mh;  // value יהיה שם הרכיב
-        option2.textContent = optionCollection.shemkupa;  // textContent יהיה שם הרכיב
-         maslul2.appendChild(option);
-    }
+      for(let r=0;r<=Math.min(optionCollection.length-1,2);r++){
+       optionCollection.sort((a, b) => b.tesuam - a.tesuam);        
+      document.getElementById(`thb${r+1}`).textContent=optionCollection[r].shemkupa;
+      document.getElementById(`output${r+1}`).textContent=optionCollection[r].tesuam+'%';
+
+       optionCollection.sort((a, b) => b.tesuam36 - a.tesuam36);
+      document.getElementById(`thb${r+11}`).textContent=optionCollection[r].shemkupa;
+      document.getElementById(`output${r+11}`).textContent=optionCollection[r].tesuam36+'%';
+
+      optionCollection.sort((a, b) => b.tesuam60 - a.tesuam60);
+      document.getElementById(`thb${r+21}`).textContent=optionCollection[r].shemkupa;
+      document.getElementById(`output${r+21}`).textContent=optionCollection[r].tesuam60+'%';
+
+       
+      } 
+    
     }
     })
     .catch(error => console.error('Error:', error));
     
     }
+

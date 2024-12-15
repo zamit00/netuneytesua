06d5014282225c348965f732d8fc291b01b76572
zamit00 <zamit00@gmail.com>
2024-12-15 11:35:52 +0200

    Update netunimscript.js

--- netunimscript.js
+++ netunimscript.js
@@ -172,7 +172,7 @@ function bring(){
         && tesuam){
 
         if (document.getElementById('rdbutton2').checked){
-         if(mozar===moz.value && (mas===maslul1.value || mas===maslul2.value )) {
+         if(mozar===moz.value && (shemkupa===maslul1.value || shemkupa===maslul2.value )) {
             optionCollection.push({mh:mhkupa, shemkupa: shemkupa,mozar:mozar, tesuam: Number(tesuam), 
               ochlosiyayaad: ochlosiyayaad, divuach: divuach, tesuam36:Number(tesuam36),tesuam60:Number(tesuam60) });
           

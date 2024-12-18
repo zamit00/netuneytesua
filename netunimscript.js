f4912152580875dd15871d16424692d13b7b7dd4
zamit00 <zamit00@gmail.com>
2024-12-18 13:04:43 +0200

    Update netunimscript.js

--- netunimscript.js
+++ netunimscript.js
@@ -1,42 +1,5 @@
-const dogma=`<Row>
- <ID>579</ID>
-<SHM_KUPA>מגדל השתלמות כללי</SHM_KUPA>
-  <TAARICH_HAFAKAT_HADOCH>09/12/2024</TAARICH_HAFAKAT_HADOCH>
-  <SUG_KUPA>קרנות השתלמות</SUG_KUPA>
-  <SHM_TAAGID_SHOLET>שלמה אליהו אחזקות בע"מ</SHM_TAAGID_SHOLET>
- <SUG_TAAGID_SHOLET>ישראלית</SUG_TAAGID_SHOLET>
- <SHM_HEVRA_MENAHELET>מגדל מקפת קרנות פנסיה וקופות גמל בע"מ</SHM_HEVRA_MENAHELET>
- <TAARICH_HAKAMA>01/01/1997</TAARICH_HAKAMA>
- <UCHLUSIYAT_YAAD>כלל האוכלוסיה</UCHLUSIYAT_YAAD>
- <HITMAHUT_RASHIT>כללי</HITMAHUT_RASHIT>
-   <HITMAHUT_MISHNIT>כללי</HITMAHUT_MISHNIT>
-  <MI_TKUFAT_DIVUACH>202311</MI_TKUFAT_DIVUACH>
- <AD_TKUFAT_DIVUACH>202410</AD_TKUFAT_DIVUACH>
-  <HAFKADOT_LLO_HAAVAROT>1776.68</HAFKADOT_LLO_HAAVAROT>    <MSHICHOT_LLO_HAAVAROT>1240.74</MSHICHOT_LLO_HAAVAROT>
-<HAAVAROT_BEIN_HAKUPOT>-1072.26</HAAVAROT_BEIN_HAKUPOT>
-<TZVIRA_NETO>-536.32</TZVIRA_NETO>
- <YITRAT_NCHASIM_LSOF_TKUFA>17191.27</YITRAT_NCHASIM_LSOF_TKUFA>
-<SHIUR_DMEI_NIHUL_AHARON>0.57</SHIUR_DMEI_NIHUL_AHARON>
-    <TSUA_MEMUZAAT_LETKUFA>1.16</TSUA_MEMUZAAT_LETKUFA>
-<TSUA_MITZTABERET_LETKUFA>14.91</TSUA_MITZTABERET_LETKUFA>
-<TSUA_MEMUZAAT_36_HODASHIM>0.33</TSUA_MEMUZAAT_36_HODASHIM>
- <TSUA_MEMUZAAT_60_HODASHIM>0.5</TSUA_MEMUZAAT_60_HODASHIM>
- <TSUA_MITZTABERET_36_HODASHIM>12.78</TSUA_MITZTABERET_36_HODASHIM>
- <TSUA_MITZTABERET_60_HODASHIM>35.1</TSUA_MITZTABERET_60_HODASHIM>
- <TSUA_SHNATIT_MEMUZAAT_3_SHANIM>4.09</TSUA_SHNATIT_MEMUZAAT_3_SHANIM>
-<TSUA_SHNATIT_MEMUZAAT_5_SHANIM>6.2</TSUA_SHNATIT_MEMUZAAT_5_SHANIM>
-<STIAT_TEKEN_36_HODASHIM>1.69</STIAT_TEKEN_36_HODASHIM>
-<STIAT_TEKEN_60_HODASHIM>2.03</STIAT_TEKEN_60_HODASHIM>
-<MATZAV_DIVUACH>דווח</MATZAV_DIVUACH>
-<ALPHA_SHNATIT>1.44</ALPHA_SHNATIT>
-<SHARP_ANAF>0.36</SHARP_ANAF>
-<SHARP_KOL_HAKUPOT>0.6</SHARP_KOL_HAKUPOT>
-<SHARP_RIBIT_HASRAT_SIKUN>0.68</SHARP_RIBIT_HASRAT_SIKUN>
-<YAHAS_NEZILUT>74.0</YAHAS_NEZILUT>
- <MISPAR_KUPA_AV>0</MISPAR_KUPA_AV>
-<NUM_HEVRA>512237744</NUM_HEVRA>
- </Row>`
 
+let newWindow = null;
 const tkokadivuach="נכון לחודש 10/2024";
  document.getElementById("tkofatdivuachn").innerText=tkokadivuach;
 
@@ -114,10 +77,289 @@ var rashit;
   
 }
 
+function sayok(x){
+  const mhkupaf= x.parentNode.parentNode.firstElementChild.innerText;
+  
+  fetch('kupotKlali.xml')
+    .then(response => response.text()) // מקבל את תוכן הקובץ כטקסט
+    .then(xmlString => {
+      const parser = new DOMParser(); // יוצר parser לניתוח XML
+      const xmlDoc = parser.parseFromString(xmlString, "application/xml"); // מנתח את הטקסט למבנה XML
+ 
 
+      const rows = xmlDoc.getElementsByTagName("Row");
+      for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        const mhkupa = row.getElementsByTagName("ID")[0]?.textContent ||  '';
+        
+        if(mhkupa && Number(mhkupa)===Number(mhkupaf)){
+        
+       
+     
+      const mozar = row.getElementsByTagName("SUG_KUPA")[0]?.textContent ||  '';
+
+     
+      const shemkupa = row.getElementsByTagName("SHM_KUPA")[0]?.textContent || ''; 
+      const mas = row.getElementsByTagName("HITMAHUT_MISHNIT")[0]?.textContent ||  '';
+      const tesuam = row.getElementsByTagName("TSUA_MITZTABERET_LETKUFA")[0]?.textContent ||  '';
+      const tesuam36 = row.getElementsByTagName("TSUA_MITZTABERET_36_HODASHIM")[0]?.textContent ||  '';
+      const tesuam60 = row.getElementsByTagName("TSUA_MITZTABERET_60_HODASHIM")[0]?.textContent ||  '';
+      const menahelet = row.getElementsByTagName("SHM_HEVRA_MENAHELET")[0]?.textContent || '';
+      const rashit=row.getElementsByTagName("HITMAHUT_RASHIT")[0]?.textContent || '';
+      const yitratnechasim=row.getElementsByTagName("YITRAT_NCHASIM_LSOF_TKUFA")[0]?.textContent || '';
+      const shiurdn=row.getElementsByTagName("SHIUR_DMEI_NIHUL_AHARON")[0]?.textContent || '';
+      const stiya36=row.getElementsByTagName("STIAT_TEKEN_36_HODASHIM")[0]?.textContent || '';
+      const stiya60=row.getElementsByTagName("STIAT_TEKEN_60_HODASHIM")[0]?.textContent || '';
+
+      const screenw = window.innerWidth;
+
+
+      const screenh = window.innerHeight;
+      const maxw = Math.min(screenw * 0.95, 800);
+  const maxh = Math.min(screenh * 0.95, 600);
+  const windowf = `width=${maxw},height=${maxh},resizable=yes,scrollbars=yes`;
+  
+  // פתיחת החלון החדש
+  if (!newWindow || newWindow.closed) {
+    newWindow = window.open('', '_blank', windowf);  // פותחים חלון חדש אם הוא לא פתוח
+  } else {
+    newWindow.document.body.innerHTML = '';  // מנקים את התוכן הקודם בחלון
+  }
+
+  
+  const tableHTML = `
+    <html dir="rtl">
+    <head>
+      <title style="text-align:center;"></title>
+      <style>
+        th{background-color:#333;color:white;text-align: right; padding-right:5px;}
+        td{padding-right:5px;}
+        tr{height:20px;width:90vw;}
+        .thsmallinfo{max-width:30vw;text-align:center;}
+        .thr{text-align:right;padding-right:5px;}
+        .thbiginfo{width:45vw;}
+        .oc{background-color:rgb(163, 157, 157);color:rgb(89, 227, 89);font-weight:bold;}
+      </style>
+    </head>
+    <body style="display:flex;flex-direction:column; align-items:center;">
+      <table id="tbl1" border="1" class="tblinfo" style="height:auto;width:90vw;margin-bottom:2vw;
+      box-sizing:border-box;border-collapse:collapse">
+        
+          <tr class="trinfo">
+            <th class="thbiginfo" >שם קופה</th>
+            <td class="thbiginfo" ></td>
+          </tr>
+        
+          <tr class="trinfo" >
+            <th class="thbiginfo" >מספר מה</th>
+            <td class="thbiginfo" ></td>
+          </tr>
+          
+          <tr class="trinfo" >
+            <th class="thbiginfo" >שם חברה מנהלת</th>
+            <td class="thbiginfo" ></td>
+          </tr>
+         <tr class="trinfo" >
+            <th class="thbiginfo" >התמחות ראשית</th>
+            <td class="thbiginfo" ></td>
+          </tr>
+        
+          <tr class="trinfo" >
+            <th class="thbiginfo" >התמחות משנית</th>
+            <td class="thbiginfo" ></td>
+          </tr>
+          
+          <tr class="trinfo" >
+            <th class="thbiginfo" >תשואה 12 חודשים</th>
+            <td class="thbiginfo" ></td>
+          </tr>
+          <tr class="trinfo" >
+            <th class="thbiginfo" >תשואה 3 שנים</th>
+            <td class="thbiginfo" ></td>
+          </tr>
+        
+          <tr class="trinfo" >
+            <th class="thbiginfo" >תשואה 5 שנים</th>
+            <td class="thbiginfo" ></td>
+          </tr>
+          
+          <tr class="trinfo" >
+            <th class="thbiginfo" >יתרת נכסים</th>
+            <td class="thbiginfo" ></td>
+          </tr>
+          <tr class="trinfo" >
+            <th class="thbiginfo" >שיעור דמי ניהול</th>
+            <td class="thbiginfo" ></td>
+          </tr>
+        
+          <tr class="trinfo" >
+            <th class="thbiginfo" >סטיית תקן 3 שנים</th>
+            <td class="thbiginfo" ></td>
+          </tr>
+          
+          <tr class="trinfo" >
+            <th class="thbiginfo" >סטיית תקן 5 שנים</th>
+            <td class="thbiginfo" ></td>
+          </tr>
+
+      </table>
+      <table id="tbl2" border="1" class="tblinfo" style="height:auto;max-width:90vw;margin-bottom:10px;
+      box-sizing:border-box;border-collapse:collapse"">
+          <tr class="trinfo">
+            <th class="thsmallinfo thr oc" style="min-width:45vw;">סוג הנכס</th>
+            <th class="thsmallinfo oc" style="min-width:20vw;">סכום באלפי ש"ח</th>
+            <th class="thsmallinfo oc" style="min-width:20vw;">שיעור מהנכסים</th>
+          </tr>
+          <tr class="trinfo">
+            <th class="thsmallinfo thr" style="min-width:45vw;">נכסים סחירים ונזילים </th>
+            <td class="thsmallinfo" style="min-width:20vw;"></td>
+            <td class="thsmallinfo" style="min-width:20vw;"></td>
+          </tr>
+        
+          <tr class="trinfo" >
+            <th class="thsmallinfo thr" >נכסים לא סחירים </th>
+            <td class="thsmallinfo" ></td>
+            <td class="thsmallinfo" ></td>
+          </tr>
+          
+          <tr class="trinfo" >
+            <th class="thsmallinfo thr" > נכסים בארץ </th>
+            <td class="thsmallinfo" ></td>
+            <td class="thsmallinfo" ></td>
+          </tr>
+          <tr class="trinfo" >
+            <th class="thsmallinfo thr" >נכסים בחול </th>
+            <td class="thsmallinfo" ></td>
+            <td class="thsmallinfo" ></td>
+          </tr>
+        </table>
+
+        <table id="tbl3" border="1" class="tblinfo" style="height:auto;max-width:90vw;margin-bottom:10px;
+        box-sizing:border-box;border-collapse:collapse"" >
+          <tr class="trinfo">
+            <th class="thsmallinfo thr oc" style="min-width:45vw;">סוג החשיפה</th>
+            <td class="thsmallinfo oc" style="min-width:20vw;">סכום באלפי ש"ח</td>
+            <td class="thsmallinfo oc" style="min-width:20vw;">שיעור מהנכסים</td>
+          </tr>
+          <tr class="trinfo" >
+            <th class="thsmallinfo thr" style="min-width:45vw;">חשיפה למניות   </th>
+            <td class="thsmallinfo" style="min-width:20vw;"></td>
+             <td class="thsmallinfo" style="min-width:20vw;"></td>
+          </tr>
+        
+          <tr class="trinfo" >
+            <th class="thsmallinfo thr" > חשיפה למטבע חוץ  </th>
+            <td class="thsmallinfo" ></td>
+             <td class="thsmallinfo" ></td>
+          </tr>
+          
+          <tr class="trinfo" >
+            <th class="thsmallinfo thr" > חשיפה לחול </th>
+            <td class="thsmallinfo" ></td>
+             <td class="thsmallinfo" ></td>
+          </tr>
+       
+        </table>
+
+    </body>
+    </html>
+  `;
+
+ 
+  newWindow.document.write(tableHTML);
+  newWindow.document.title ="נדרש לסגור את החלון בסיום"
+
+  const tbl1 = newWindow.document.getElementById('tbl1');
+
+  const values = [shemkupa,mhkupa, menahelet,rashit,mas, tesuam+"%", tesuam36+"%", tesuam60+"%",  
+     yitratnechasim+" מליוני שח", shiurdn+"%", stiya36, stiya60];
+  const rows = tbl1.getElementsByTagName('tr');
+
+  for (let i = 0; i < rows.length; i++) {  
+    const row = rows[i];
+    const cell = row.cells[1];
+    if (i - 1 < values.length) {  
+      cell.textContent = values[i];
+    }
+}
+  
+  
+
+
+
+
+fetch('nechasim.xml')
+    .then(response => response.text()) 
+    .then(nechsimstring => {
+      const parser = new DOMParser(); 
+      const xmlNechasim = parser.parseFromString(nechsimstring, "application/xml"); // מנתח את הטקסט למבנה XML
+      
+      
+      const tbl2 = newWindow.document.getElementById('tbl2');
+      const tbl3 = newWindow.document.getElementById('tbl3');
+
+      const rows = xmlNechasim.getElementsByTagName("Row");
+      for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        const mhkupa = row.getElementsByTagName("ID_KUPA")[0]?.textContent ||  '';
+        
+        
+        const sugneches = row.getElementsByTagName("ID_SUG_NECHES")[0]?.textContent ||  '';
+        const schumsugneches = row.getElementsByTagName("SCHUM_SUG_NECHES")[0]?.textContent ||  '';
+        const ahuzsugneches = row.getElementsByTagName("ACHUZ_SUG_NECHES")[0]?.textContent ||  '';
+
+
+        if(mhkupa && Number(mhkupa)===Number(mhkupaf) ){
+
+          if(Number(sugneches)===4721){tbl2.rows[1].cells[1].textContent =Number(schumsugneches).toLocaleString();
+            tbl2.rows[1].cells[2].textContent =Number(ahuzsugneches)+"%";
+          }
+          if(Number(sugneches)===4722){tbl2.rows[2].cells[1].textContent =Number(schumsugneches).toLocaleString();
+            tbl2.rows[2].cells[2].textContent =Number(ahuzsugneches)+"%";
+          }
+          if(Number(sugneches)===4731){tbl2.rows[3].cells[1].textContent =Number(schumsugneches).toLocaleString();
+            tbl2.rows[3].cells[2].textContent =Number(ahuzsugneches)+"%";
+          }
+          if(Number(sugneches)===4732){tbl2.rows[4].cells[1].textContent =Number(schumsugneches).toLocaleString();
+            tbl2.rows[4].cells[2].textContent =Number(ahuzsugneches)+"%";
+          }
+          if(Number(sugneches)===4751){tbl3.rows[1].cells[1].textContent =Number(schumsugneches).toLocaleString();
+            tbl3.rows[1].cells[2].textContent =Number(ahuzsugneches)+"%";
+          }
+          if(Number(sugneches)===4752){tbl3.rows[2].cells[1].textContent =Number(schumsugneches).toLocaleString();
+            tbl3.rows[2].cells[2].textContent =Number(ahuzsugneches)+"%";
+          }
+          if(Number(sugneches)===4761){tbl3.rows[3].cells[1].textContent =Number(schumsugneches).toLocaleString();
+            tbl3.rows[3].cells[2].textContent =Number(ahuzsugneches)+"%";
+          }
+
+
+
+        }
+      }
+  
+      })
+      .catch(error => console.error('Error:', error));
+  
+  
+  
+  
+  newWindow.document.close();
+  
+
+
+
+     /* console.log(mhkupa+":"+shemkupa +":"+ mozar+":"+ tesuam+":"+tesuam36 +":"+
+        tesuam60 +":"+yitratnechasim +":"+shiurdn +":"+ stiya36+":"+menahelet +":"+stiya60)*/
+        }
+      }
+
+    })
+    .catch(error => console.error('Error:', error));
+}
 
 function bring(){
- if(maslul.value===''){return;}
+ if(maslul.value===''){alert("בחר מסלול השקעה");return;}
  
  
  document.getElementById('table12').style.display="block";
@@ -127,10 +369,15 @@ function bring(){
  for(let i=1;i<=3;i++){
      document.getElementById(`thb${i}`).textContent='';
      document.getElementById(`output${i}`).textContent='';
+     document.getElementById(`outputmh${i}`).textContent='';
+     
      document.getElementById(`thb${i+10}`).textContent='';
      document.getElementById(`output${i+10}`).textContent='';
+     document.getElementById(`outputmh${i+10}`).textContent='';
+
      document.getElementById(`thb${i+20}`).textContent='';
      document.getElementById(`output${i+20}`).textContent='';
+     document.getElementById(`outputmh${i+20}`).textContent='';
 
  
  }
@@ -153,17 +400,24 @@ function bring(){
         
         
         
-        const yitratnehasim = row.getElementsByTagName("YITRAT_NCHASIM_LSOF_TKUFA")[0]?.textContent || '';
-        const divuach=  row.getElementsByTagName("MATZAV_DIVUACH")[0]?.textContent ||  '';
-        const ochlosiyayaad=row.getElementsByTagName("UCHLUSIYAT_YAAD")[0]?.textContent ||  '';
-        const mozar = row.getElementsByTagName("SUG_KUPA")[0]?.textContent ||  '';
+      const yitratnehasim = row.getElementsByTagName("YITRAT_NCHASIM_LSOF_TKUFA")[0]?.textContent || '';
+      const divuach=  row.getElementsByTagName("MATZAV_DIVUACH")[0]?.textContent ||  '';
+      const ochlosiyayaad=row.getElementsByTagName("UCHLUSIYAT_YAAD")[0]?.textContent ||  '';
+      const mozar = row.getElementsByTagName("SUG_KUPA")[0]?.textContent ||  '';
 
-        const shemkupa = row.getElementsByTagName("SHM_KUPA")[0]?.textContent || '';
-        const mhkupa = row.getElementsByTagName("ID")[0]?.textContent ||  '';
-        const mas = row.getElementsByTagName("HITMAHUT_MISHNIT")[0]?.textContent ||  '';
-        const tesuam = row.getElementsByTagName("TSUA_MITZTABERET_LETKUFA")[0]?.textContent ||  '';
-       const tesuam36 = row.getElementsByTagName("TSUA_MITZTABERET_36_HODASHIM")[0]?.textContent ||  '';
-       const tesuam60 = row.getElementsByTagName("TSUA_MITZTABERET_60_HODASHIM")[0]?.textContent ||  '';
+      const shemkupa = row.getElementsByTagName("SHM_KUPA")[0]?.textContent || '';
+      const mhkupa = row.getElementsByTagName("ID")[0]?.textContent ||  '';
+      const mas = row.getElementsByTagName("HITMAHUT_MISHNIT")[0]?.textContent ||  '';
+      const tesuam = row.getElementsByTagName("TSUA_MITZTABERET_LETKUFA")[0]?.textContent ||  '';
+      const tesuam36 = row.getElementsByTagName("TSUA_MITZTABERET_36_HODASHIM")[0]?.textContent ||  '';
+      const tesuam60 = row.getElementsByTagName("TSUA_MITZTABERET_60_HODASHIM")[0]?.textContent ||  '';
+      const menahelet = row.getElementsByTagName("SHM_HEVRA_MENAHELET")[0]?.textContent || '';
+      const rashit=row.getElementsByTagName("HITMAHUT_RASHIT")[0]?.textContent || '';
+      const yitratnechasim=row.getElementsByTagName("YITRAT_NCHASIM_LSOF_TKUFA")[0]?.textContent || '';
+      const shiurdn=row.getElementsByTagName("SHIUR_DMEI_NIHUL_AHARON")[0]?.textContent || '';
+      const stiya36=row.getElementsByTagName("STIAT_TEKEN_36_HODASHIM")[0]?.textContent || '';
+      const stiya60=row.getElementsByTagName("STIAT_TEKEN_60_HODASHIM")[0]?.textContent || '';
+
      
         if(Number(yitratnehasim)>0 &&  divuach ==="דווח" && !shemkupa.includes('ניהול אישי') && !shemkupa.includes('IRA') &&
         ochlosiyayaad!== "עובדי סקטור מסויים" && ochlosiyayaad!==`עובדי מפעל/גוף מסויים`
@@ -192,16 +446,25 @@ function bring(){
      
       if(optionCollection.length!==0){
       for(let r=0;r<=Math.min(optionCollection.length-1,2);r++){
-       optionCollection.sort((a, b) => b.tesuam - a.tesuam);        
-      document.getElementById(`thb${r+1}`).textContent=optionCollection[r].shemkupa;
-      document.getElementById(`output${r+1}`).textContent=optionCollection[r].tesuam+'%';
+       optionCollection.sort((a, b) => b.tesuam - a.tesuam); 
+       document.getElementById(`outputmh${r+1}`).textContent=optionCollection[r].mh;      
+       document.getElementById(`thb${r+1}`).innerHTML=optionCollection[r].shemkupa
+       +      `<i class="fa fa-info-circle"  id='t12_${r+1}' style="font-size: 14px;float:left;margin-left:5px"
+        onclick="sayok(this)"></i>`;
+       document.getElementById(`output${r+1}`).textContent=optionCollection[r].tesuam+'%';
 
        optionCollection.sort((a, b) => b.tesuam36 - a.tesuam36);
-      document.getElementById(`thb${r+11}`).textContent=optionCollection[r].shemkupa;
-      document.getElementById(`output${r+11}`).textContent=optionCollection[r].tesuam36+'%';
+       document.getElementById(`outputmh${r+11}`).textContent=optionCollection[r].mh; 
+       document.getElementById(`thb${r+11}`).innerHTML=optionCollection[r].shemkupa
+       +      `<i class="fa fa-info-circle"  id='t36_${r+1}' style="font-size: 14px;float:left;margin-left:5px"
+        onclick="sayok(this)"></i>`;
+       document.getElementById(`output${r+11}`).textContent=optionCollection[r].tesuam36+'%';
 
       optionCollection.sort((a, b) => b.tesuam60 - a.tesuam60);
-      document.getElementById(`thb${r+21}`).textContent=optionCollection[r].shemkupa;
+      document.getElementById(`outputmh${r+21}`).textContent=optionCollection[r].mh; 
+      document.getElementById(`thb${r+21}`).innerHTML=optionCollection[r].shemkupa
+      +      `<i class="fa fa-info-circle"  id='t60_${r+1}' style="font-size: 14px;float:left;margin-left:5px"
+       onclick="sayok(this)"></i>`;
       document.getElementById(`output${r+21}`).textContent=optionCollection[r].tesuam60+'%';
 
        
